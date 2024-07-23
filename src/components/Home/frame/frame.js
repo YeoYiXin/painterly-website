5b6055c79d2f7df18fa4152f67bb58a81db07aa9
eche0083 <129277881+rincyl@users.noreply.github.com>
2024-07-23 15:50:24 +0800

    Added bounce animation to web name in frame.js Adjusted upload box width in upload.js Adjusted download box width in download.js Adjusted filter option box width in filter.js

--- src/components/Home/frame/frame.js
+++ src/components/Home/frame/frame.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Frame = () => {
   return (
-    <div className="flex items-left justify-left align-middle pt-10 pl-[20vw]">
+    <div className="flex items-left justify-left align-middle pt-10 pl-[15vw]">
       <div className="flex flex-row items-center gap-8">
         <video
           className="w-[20vw] h-full rounded-3xl"
@@ -12,33 +12,24 @@ const Frame = () => {
         >
           <source src="video.mp4" type="video/mp4" />
         </video>
-        <div className="flex items-center justify-center h-1/2 gap-10 md:gap-10 z-100">
-          <div>
-            <span className="font-bold text-4xl md:text-5xl">P</span>
+        <div className="flex items-center justify-center h-1/2 gap-10 md:gap-10 z-100 text-[#626681]">
+          <div className="font-bold text-4xl md:text-5xl animate-bounce">P
           </div>
-          <div>
-            <span className="font-bold text-4xl md:text-5xl">A</span>
+          <div className="font-bold text-4xl md:text-5xl animate-bounce">A
           </div>
-          <div>
-            <span className="font-bold text-4xl md:text-5xl">I</span>
+          <div className="font-bold text-4xl md:text-5xl animate-bounce">I
           </div>
-          <div>
-            <span className="font-bold text-4xl md:text-5xl">N</span>
+          <div className="font-bold text-4xl md:text-5xl animate-bounce">N
           </div>
-          <div>
-            <span className="font-bold text-4xl md:text-5xl">T</span>
+          <div className="font-bold text-4xl md:text-5xl animate-bounce">T
           </div>
-          <div>
-            <span className="font-bold text-4xl md:text-5xl">E</span>
+          <div className="font-bold text-4xl md:text-5xl animate-bounce">E
           </div>
-          <div>
-            <span className="font-bold text-4xl md:text-5xl">R</span>
+          <div className="font-bold text-4xl md:text-5xl animate-bounce">R
           </div>
-          <div>
-            <span className="font-bold text-4xl md:text-5xl">L</span>
+          <div className="font-bold text-4xl md:text-5xl animate-bounce">L
           </div>
-          <div>
-            <span className="font-bold text-4xl md:text-5xl">Y</span>
+          <div className="font-bold text-4xl md:text-5xl animate-bounce">Y
           </div>
         </div>
       </div>
--- src/components/get_started/filter/filter.js
+++ src/components/get_started/filter/filter.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Filter = ({onFilterChange}) => {
+const Filter = ({ onFilterChange }) => {
   const [selected, setSelected] = useState(null);
 
   const isSelected = (filter) => selected === filter;
@@ -11,8 +11,8 @@ const Filter = ({onFilterChange}) => {
   };
 
   return (
-    <div className="w-full h-[10rem] flex-col items-center justify-center mb-10 mt-10">
-      <div className="border w-full h-full grid grid-cols-3 place-items-center">
+    <div className="w-[50vw] h-[10rem] flex-col justify-center mb-10 mt-10 align-middle items-center">
+      <div className="w-[50vw] h-full grid grid-cols-3 place-items-center rounded-xl border-[#171955] border-4">
         {/* Cartoon */}
         <div
           className={`cursor-pointer text-sm md:text-base py-4 px-6 md:px-10 ${isSelected("cartoon") ? "bg-lightPink" : "bg-gray-300"}`}
