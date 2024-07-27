498271fb12e67e9f35ebfee36c57ab80065d1908
eche0083 <129277881+rincyl@users.noreply.github.com>
2024-07-28 02:23:41 +0800

    Adjusted padding for container.js Adjusted width responsiveness for details.js

--- src/components/gallery/details.js
+++ src/components/gallery/details.js
@@ -2,13 +2,12 @@ const Details = ({ isOpen, onClose, children }) => {
 
   return (
     <div
-      className={`pt-16 fixed inset-0 bg-black bg-opacity-50 grid place-items-center ${
-        isOpen ? "" : "hidden"
-      }`}
+      className={`pt-16 fixed inset-0 bg-black bg-opacity-50 grid place-items-center ${isOpen ? "" : "hidden"
+        }`}
       onClick={onClose}
     >
       <div
-        className="bg-white p-5 shadow-md flex flex-col gap-5 w-4/5"
+        className="bg-white p-5 shadow-md flex flex-col gap-5 lg:w-[40vw] md:w-[40vw] sm:w-[40vw] w-[70vw]"
         onClick={(e) => e.stopPropagation()}
       >
         {children}
