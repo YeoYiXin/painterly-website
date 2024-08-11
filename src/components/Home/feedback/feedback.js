7879edc566cfd42d6233f50c9b00d5dbea92f07e
YeoYiXin <sara.yeo.yixin@gmail.com>
2024-08-11 23:33:12 +0800

    added contact information, homepage and  gallery images

--- src/components/Home/feedback/feedback.js
+++ src/components/Home/feedback/feedback.js
@@ -4,38 +4,37 @@ const Feedback = () => {
   const feedbacks = [
     {
       id: 1,
-      imageUrl: "https://via.placeholder.com/150",
-      username: "username",
-      feedback:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed doeiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim adminim veniam, quis nostrud exercitation ullamco laboris nisi ualiquip ex ea commodo consequat. <br/> Duis aute irure dolor inreprehenderit in voluptate velit esse cillum dolore eu fugiat nullapariatur. Excepteur sint occaecat cupidatat non proident, sunt inculpa qui officia deserunt mollit anim id est laborum.",
+      imageUrl: "/gallery/cat.png",
+      username: "Yeo Yi Xin",
+      feedback: "Such a stunning effect! Absolutely loving it!",
     },
     {
       id: 2,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "/gallery/wed.png",
       username: "username",
       feedback: "feedback",
     },
     {
       id: 3,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "/gallery/flower.png",
       username: "username",
       feedback: "feedback",
     },
     {
       id: 4,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "/gallery/sloth.png",
       username: "username",
       feedback: "feedback",
     },
     {
       id: 5,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "/gallery/scenery.png",
       username: "username",
       feedback: "feedback",
     },
     {
       id: 6,
-      imageUrl: "https://via.placeholder.com/150",
+      imageUrl: "/gallery/ts.png",
       username: "username",
       feedback: "feedback",
     },
